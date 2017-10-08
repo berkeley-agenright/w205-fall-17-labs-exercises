ab6e38bf02252257febc974d6501875365d1ffd3
root <root@ip-172-31-12-244.ec2.internal>
2017-10-08 15:16:08 +0000

    Data load script, first part of exercise #1

---
+++ exercise_1/loading_and_modelling/load_data_lake.sh
@@ -0,0 +1,25 @@
+!/bin/bash
+
+# must be run as w205 - "su - w205" prior to running this script
+
+unzip Hospital_Revised_Flatfiles.zip
+for name in *\ *.csv; do tail -n +2 "$name" > "${name// /}"; mv -v "$name" "$name".orig; done
+
+#
+#rename the needed files
+#
+mv -v HospitalGeneralInformation.csv hospitals.csv
+mv -v TimelyandEffectiveCare-Hospital.csv effective_care.csv
+mv -v ReadmissionsandDeaths-Hospital.csv readmissions.csv
+mv -v MeasureDates.csv Measures.csv
+mv -v hvbp_hcahps_05_28_2015.csv surveys_responses.csv
+
+#
+# Create the hdfs folder and move files there
+#
+hdfs dfs -mkdir /user/w205/hospital_compare
+hdfs dfs -put hospitals.csv /user/w205/hospital_compare
+hdfs dfs -put effective_care.csv /user/w205/hospital_compare
+hdfs dfs -put readmissions.csv /user/w205/hospital_compare
+hdfs dfs -put Measures.csv /user/w205/hospital_compare
+hdfs dfs -put surveys_responses.csv /user/w205/hospital_compare
